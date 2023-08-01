87c0940886ebf9e7b0149fd43af799a01eaa5f37
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-07-31 17:15:34 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import {useEffect,useState} from "react"
+// import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+
+
+const BASEURL = "https://pokeapi.co/api/v2"
 
 function App() {
+  const [pokemons,setPokemons]=useState([])
+  useEffect(()=>{
+
+async function getPokes (URL){
+try {
+  console.log("test 10")
+   const result= await axios.get(`${URL}/pokemon`);
+   console.log(result)  
+   const {data} = result 
+  //  console.log(data)
+  setPokemons(data.results)
+
+} catch (error) {
+    console.log(error)
+}}
+console.log("test 16")
+getPokes(BASEURL)
+  },[])
+
+console.log(pokemons)  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">{
+      pokemons.map((pokemon)=>{
+        return <p key={pokemon.name}>{pokemon.name} </p>
+      })
+    }
     </div>
   );
 }
